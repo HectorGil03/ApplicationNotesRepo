808d2eaabd97c9ae963b02743d4bf0c0aa70d87b
CITASONNET\hector.gil <hector.gil@saludonnet.com>
2024-04-30 12:11:02 +0200

    add repo

--- ApplicationNotes/ApplicationNotes/obj/Debug/net8.0/ApplicationNotesApi.AssemblyInfo.cs
+++ ApplicationNotes/ApplicationNotes/obj/Debug/net8.0/ApplicationNotesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationNotesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0506a2866e139c27b9e307df9d407830f12e6fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationNotesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationNotesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ApplicationNotes/Notes/obj/Debug/net8.0/Notes.AssemblyInfo.cs
+++ ApplicationNotes/Notes/obj/Debug/net8.0/Notes.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0506a2866e139c27b9e307df9d407830f12e6fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
